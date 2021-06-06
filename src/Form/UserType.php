cad1def8f0467a6599c7226c3f63c6ad2570fded
t.zemmari <tarik.zemmari@xabec.es>
2021-06-06 20:01:14 +0200

    añadido builder de roles

---
+++ src/Form/UserType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles',ChoiceType::class, [
+                "required" => true,
+                "multiple"=> false,
+                "expanded"=> false,
+                "choices"=> [
+                    "User"=> "ROLE_USER",
+                    "Admin"=>"ROLE_ADMIN"
+                ]
+            ])
+            ->add('password')
+            ->add('web')
+        ;
+
+        $builder->get('roles')->addModelTransformer(new CallbackTransformer(
+
+            function ($rolesArray) {
+                return count($rolesArray) ? $rolesArray[0] : null;
+            },
+            function($rolesString) {
+                return [$rolesString];
+            }
+        ))
+        ;
+    
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
